9cf6e2b4e4f24a18bc4d0522e247a3900fea2a19
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-10-04 17:30:13 -0300

    Implementado update, delete da tela apartamento e ajustado layout.

--- js/apartamento.js
+++ js/apartamento.js
@@ -11,7 +11,7 @@ function show(apartamentos) {
             <th scope="col">Andar</th>
             <th scope="col">Bloco</th>
             <th scope="col">Status</th>
-            <th scope="col">Ações</th>
+
         </tr>
     </thead>
     `;
@@ -19,13 +19,12 @@ function show(apartamentos) {
     for (let apartamento of apartamentos) {
         tab +=
             `
-        <tr>
+        <tr onclick="preencherFormulario(this)">
             <td scope="row">${apartamento.apartamento_id}</td>
             <td>${apartamento.numero}</td>
             <td>${apartamento.andar}</td>
             <td>${apartamento.bloco}</td>
             <td>${apartamento.status}</td>
-            <td><i class="bi bi-pencil-square"></i><i class="bi bi-trash3-fill"></i></td>
         </tr>
         `;
     }
@@ -38,7 +37,7 @@ async function getAPI(url) {
 
     var data = await response.json();
     console.log(data);
-    
+
     if (response) {
         show(data);
     }
@@ -50,35 +49,68 @@ getAPI(url);
 //ENVIAR OS DADOS DO FORMULÁRIO PARA CADASTRO
 
 document.getElementById("btn-cadastrar").addEventListener("click", async () => {
+    const id = document.getElementById("id").value;
     const numero = document.getElementById("numero").value;
     const andar = document.getElementById("andar").value;
     const bloco = document.getElementById("bloco").value;
     const status = document.getElementById("select-cadastro").value;
 
-    const data = {
-        numero,
-        andar,
-        bloco,
-        status
-    };
+    if (id > 0) { //ENVIA PARA ATUALIZAR OS DADOS SE O ID FOR MAIOR QUE 0
 
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
+        console.log("ID PREENCHIDO")
 
-        if (response.ok) {
-            alert("Dados cadastrados com sucesso!");
-            getAPI(url);
-        } else {
-            alert("Erro ao cadastrar os dados.");
+        const data = {
+            numero,
+            andar,
+            bloco,
+            status
+        };
+
+        try {
+            const response = await fetch(url + "/" + id, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                alert("Dados atualizados com sucesso!");
+                getAPI(url);
+            } else {
+                alert("Erro ao atualizar os dados.");
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
         }
-    } catch (error) {
-        console.error("Erro na requisição:", error);
+
+    } else { //ENVIA OS DADOS DO FORMULÁRIO PARA CADASTRO
+        const data = {
+            numero,
+            andar,
+            bloco,
+            status
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                alert("Dados cadastrados com sucesso!");
+                getAPI(url);
+            } else {
+                alert("Erro ao cadastrar os dados.");
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+        }
     }
 });
 
@@ -88,7 +120,69 @@ document.getElementById("btn-cadastrar").addEventListener("click", async () => {
 
 function limparCampos() {
     document.getElementById("numero").value = "";
-    document.getElementById("andar").value = "";;
-    document.getElementById("bloco").value = "";;
+    document.getElementById("andar").value = "";
+    document.getElementById("bloco").value = "";
+    document.getElementById("id").value = "",
+        document.getElementById('btn-cadastrar').textContent = 'Cadastrar'
 }
+
+//PEGAR DADOS DA LINHA DA TABELA E MOSTRAR NO FORMULÁRIO
+
+function preencherFormulario(linha) {
+    const id = linha.cells[0].textContent;
+    const numero = linha.cells[1].textContent;
+    const andar = linha.cells[2].textContent;
+    const bloco = linha.cells[3].textContent;
+
+
+
+    document.getElementById('id').value = id;
+    document.getElementById('numero').value = numero;
+    document.getElementById('andar').value = andar;
+    document.getElementById('bloco').value = bloco;
+    document.getElementById('btn-cadastrar').textContent = 'Atualizar'
+}
+
+function excluir() {
+
+    console.log("botão editar pressionado");
+}
+
+
+
+//DELETAR OS DADOS SELECIONADOS
+
+document.getElementById("btn-excluir").addEventListener("click", async () => {
+
+//EXIBE UM ALERTA PEDINDO CONFIRMAÇÃO PARA EXCLUIR OS DADOS.
+    const confirmacao = confirm("Tem certeza que deseja excluir?");
+
+    if (confirmacao) {
+
+        try {
+            const id = document.getElementById("id").value;
+            console.log(id);
+            console.log("ID PREENCHIDO")
+
+            const response = await fetch(url + "/" + id, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+
+            if (response.ok) {
+                alert("Apartamento deletado com sucesso!");
+                getAPI(url);
+            } else {
+                alert("Erro ao deletar apartamento. Confira se não existe vínculo.");
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+        }
+    }
+})
+
+
+
 
