865d569efb86d4053cb085d8dbf9d495d6231e7e
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-10-19 11:02:15 -0300

    Alterada tela inicial, agora ela abre uma nova tela para fazer o login, e a tela principal é recarregada emcima da tela de login, caso login válido.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-$(document).ready(function () {
-    $('#container-login').hide();
-});
 
-function mostrarLogin(){
-    $('#container-login').show();
-}
 function validarLogin(){
     const usuarioTeste = 'teste';
     const senhaTeste = '123'; 
@@ -12,8 +6,10 @@ function validarLogin(){
     let usuario = document.getElementById('usuario').value;
     let senha = document.getElementById('senha').value;
     if(usuarioTeste == usuario && senhaTeste == senha){
-        window.open('telaprincipal.html');
+        window.location.href = 'telaprincipal.html';
         armazenarUsuario();
+        document.getElementById("usuario").value = "";
+        document.getElementById("senha").value = "";
     }
     else{
         const myModal = new bootstrap.Modal('#modal-login', { }); 
